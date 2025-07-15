aa103e5a4e8f70bb815b8b45378970fd38fce8f9
Minhmice <trantueminh35@gmail.com>
2025-07-16 00:40:58 +0700

    Resolve merge conflict and update

--- src/app/ads/library/page.tsx
+++ src/app/ads/library/page.tsx
@@ -18,7 +18,7 @@ export default function Page() {
         } as React.CSSProperties
       }
     >
-      <AppSidebar/>
+      <AppSidebar />
       <SidebarInset>
         <SiteHeader title="Advertise Facebook" />
         <div className="p-8">
